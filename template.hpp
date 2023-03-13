dc22f6d99f1f733a7ec008364f2522822ed0810b
yutaf04 <127380146+yutaf04@users.noreply.github.com>
2023-03-13 16:35:03 +0900

    Create template.hpp

---
+++ template.hpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+#define all(xx) (xx).begin(), (xx).end()
+//const int MOD = 1000000007;
+const int MOD = 998244353;
+const int INF32 = numeric_limits<int>::max();
+const ll INF64 = numeric_limits<ll>::max();
+const int mn = 1 << 18;
+template<typename T> void chmax(T& a, const T& b){if(a < b) a = b;}
+template<typename T> void chmin(T& a, const T& b){if(a > b) a = b;}
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+}
